efe312f13e1f593490859945608a202bc4f71c1d
Sam Hollyer <rsamhollyer@gmail.com>
2021-07-21 15:23:39 -0500

    stage 2 complete

--- src/components/MemoryCard.jsx
+++ src/components/MemoryCard.jsx
@@ -2,56 +2,32 @@ import React, { Component } from 'react';
 import styled from 'styled-components';
 
 export default class MemoryCard extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isFlipped: false,
+    };
+  }
+
+  clickHandler = () => {
+    const { isFlipped } = this.state;
+    this.setState({ isFlipped: !isFlipped });
+  };
+
   render() {
+    const { isFlipped } = this.state;
     return (
       <MemoryCardBoard>
-        <MemoryCardStyles>
-          <img src="/assets/digitalcrafts-logo-white-y.png" alt="DC logo" />
-        </MemoryCardStyles>
-        <MemoryCardStyles>
-          <img src="/assets/digitalcrafts-logo-white-y.png" alt="DC logo" />
-        </MemoryCardStyles>
-        <MemoryCardStyles>
-          <img src="/assets/digitalcrafts-logo-white-y.png" alt="DC logo" />
-        </MemoryCardStyles>
-        <MemoryCardStyles>
-          <img src="/assets/digitalcrafts-logo-white-y.png" alt="DC logo" />
-        </MemoryCardStyles>
-        <MemoryCardStyles>
-          <img src="/assets/digitalcrafts-logo-white-y.png" alt="DC logo" />
-        </MemoryCardStyles>
-        <MemoryCardStyles>
-          <img src="/assets/digitalcrafts-logo-white-y.png" alt="DC logo" />
-        </MemoryCardStyles>
-        <MemoryCardStyles>
-          <img src="/assets/digitalcrafts-logo-white-y.png" alt="DC logo" />
-        </MemoryCardStyles>
-        <MemoryCardStyles>
-          <img src="/assets/digitalcrafts-logo-white-y.png" alt="DC logo" />
-        </MemoryCardStyles>
-        <MemoryCardStyles>
-          <img src="/assets/digitalcrafts-logo-white-y.png" alt="DC logo" />
-        </MemoryCardStyles>
-        <MemoryCardStyles>
-          <img src="/assets/digitalcrafts-logo-white-y.png" alt="DC logo" />
-        </MemoryCardStyles>
-        <MemoryCardStyles>
-          <img src="/assets/digitalcrafts-logo-white-y.png" alt="DC logo" />
-        </MemoryCardStyles>
-        <MemoryCardStyles>
-          <img src="/assets/digitalcrafts-logo-white-y.png" alt="DC logo" />
-        </MemoryCardStyles>
-        <MemoryCardStyles>
-          <img src="/assets/digitalcrafts-logo-white-y.png" alt="DC logo" />
-        </MemoryCardStyles>
-        <MemoryCardStyles>
-          <img src="/assets/digitalcrafts-logo-white-y.png" alt="DC logo" />
-        </MemoryCardStyles>
-        <MemoryCardStyles>
-          <img src="/assets/digitalcrafts-logo-white-y.png" alt="DC logo" />
-        </MemoryCardStyles>
-        <MemoryCardStyles>
-          <img src="/assets/digitalcrafts-logo-white-y.png" alt="DC logo" />
+        <MemoryCardStyles onClick={this.clickHandler}>
+          <MemoryCardInner isFlipped={isFlipped}>
+            <MemoryCardBack>
+              <MemoryCardImage
+                src="/assets/digitalcrafts-logo-white-y.png"
+                alt="DC logo"
+              />
+            </MemoryCardBack>
+            <MemoryCardFront>😃</MemoryCardFront>
+          </MemoryCardInner>
         </MemoryCardStyles>
       </MemoryCardBoard>
     );
@@ -69,6 +45,25 @@ const MemoryCardBoard = styled.div`
 `;
 
 const MemoryCardStyles = styled.div`
+  display: inline-block;
+  width: 150px;
+  height: 80px;
+  margin: 5px;
+  cursor: pointer;
+  position: relative;
+  perspective: 800px;
+`;
+const MemoryCardFront = styled.div`
+  font-size: 30px;
+  line-height: 20px;
+  transform: rotateY(180deg);
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  backface-visibility: hidden;
+`;
+
+const MemoryCardBack = styled.div`
   background: repeating-radial-gradient(
     circle,
     purple,
@@ -76,16 +71,27 @@ const MemoryCardStyles = styled.div`
     #4b026f 10px,
     #4b026f 20px
   );
-  display: inline-block;
-  width: 150px;
-  height: 80px;
-  margin: 3px;
-  border-radius: 5px;
-  border: solid 3px navy;
-  cursor: pointer;
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  backface-visibility: hidden;
+`;
 
-  img {
-    width: 100%;
-    padding-top: 13px;
-  }
+const MemoryCardInner = styled.div`
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  transform-style: preserve-3d;
+  transition: transform 0.8s cubic-bezier(0.175, 0.885, 0.32, 1.275);
+  border-radius: 5px;
+  border: 3px solid navy;
+  box-shadow: 0 6px 16px rgba(0, 0, 0, 0.15);
+
+  ${({ isFlipped }) => isFlipped && `transform: rotateY(180deg);`}
+`;
+const MemoryCardOuter = styled.div``;
+
+const MemoryCardImage = styled.img`
+  width: 100%;
+  padding-top: 13px;
 `;
